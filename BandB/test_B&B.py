2dce8d5bc4f2b448f42e5c7bb13f01c30c2d6aea
shaowenhan <1138500211@qq.com>
2017-10-04 15:24:15 +0800

    add three tests to test the method get_current_lp in three possible situation

--- BandB/test_B&B.py
+++ BandB/test_B&B.py
@@ -15,5 +15,84 @@ class TestSolver(TestCase):
         solution = [1.0000000000000, 2, 3, 4.5]
         self.assertEqual(s.is_int(solution), 3)
 
+    def test_get_current_lp_with_1(self):
+        """
+        max 3x1 + 4x2 + 6x3
+        x1 + x2 + x3<= 5
+        x1 >= 0
+        x2 >= 0
+        x3 >= 0
+        x3 = 1
+        """
+        origin_number = 3
+        origin_cons = [[1, 1, 1, '<=', 5],
+                [1, 0, 0, '>=', 0],
+                [0, 1, 0, '>=', 0],
+                [0, 0, 1, '>=', 0]]
+        origin_obj = ['max', 3, 4, 6]
+        fixed = [-1, -1, 1]
+        s = bandb.BandB(origin_number, origin_cons, origin_obj)
+        number, cons, obj, constant = s.get_current_lp(fixed)
+        self.assertEqual(number, 2)
+        self.assertEqual(cons, [[1, 1, '<=', 4],
+                                [1, 0, '>=', 0],
+                                [0, 1, '>=', 0],
+                                [0, 0, '>=', -1]])
+        self.assertEqual(obj, ['max', 3, 4])
+        self.assertEqual(constant, 6)
+
+    def test_get_current_lp_with_0(self):
+        """
+        max 3x1 + 4x2 + 6x3
+        x1 + x2 + x3<= 5
+        x1 >= 0
+        x2 >= 0
+        x3 >= 0
+        x3 = 0
+        """
+        origin_number = 3
+        origin_cons = [[1, 1, 1, '<=', 5],
+                       [1, 0, 0, '>=', 0],
+                       [0, 1, 0, '>=', 0],
+                       [0, 0, 1, '>=', 0]]
+        origin_obj = ['max', 3, 4, 6]
+        fixed = [-1, -1, 0]
+        s = bandb.BandB(origin_number, origin_cons, origin_obj)
+        number, cons, obj, constant = s.get_current_lp(fixed)
+        self.assertEqual(number, 2)
+        self.assertEqual(cons, [[1, 1, '<=', 5],
+                                [1, 0, '>=', 0],
+                                [0, 1, '>=', 0],
+                                [0, 0, '>=', 0]])
+        self.assertEqual(obj, ['max', 3, 4])
+        self.assertEqual(constant, 0)
+
+    def test_get_current_lp_with_0_and_1(self):
+        """
+        max 3x1 + 4x2 + 6x3
+        x1 + x2 + x3<= 5
+        x1 >= 0
+        x2 >= 0
+        x3 >= 0
+        x2 = 0
+        x3 = 1
+        """
+        origin_number = 3
+        origin_cons = [[1, 1, 1, '<=', 5],
+                       [1, 0, 0, '>=', 0],
+                       [0, 1, 0, '>=', 0],
+                       [0, 0, 1, '>=', 0]]
+        origin_obj = ['max', 3, 4, 6]
+        fixed = [-1, 0, 1]
+        s = bandb.BandB(origin_number, origin_cons, origin_obj)
+        number, cons, obj, constant = s.get_current_lp(fixed)
+        self.assertEqual(number, 1)
+        self.assertEqual(cons, [[1, '<=', 4],
+                                [1, '>=', 0],
+                                [0, '>=', 0],
+                                [0, '>=', -1]])
+        self.assertEqual(obj, ['max', 3])
+        self.assertEqual(constant, 6)
+
 if __name__ == '__main__':
     main()
